58a8c7658a2ff18554187542c96ae18e23e0de5b
dettlaffb <209295@edu.p.lodz.pl>
2019-05-22 15:18:20 +0200

    Dodanie loggera do klasy KeyDownListener

--- src/main/java/edu/kis/powp/jobs2d/events/KeyDownListener.java
+++ src/main/java/edu/kis/powp/jobs2d/events/KeyDownListener.java
@@ -2,8 +2,11 @@ package edu.kis.powp.jobs2d.events;
 
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.util.logging.Logger;
 
 public class KeyDownListener implements KeyListener {
+
+    private final static Logger logger = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
 
     @Override
     public void keyPressed(KeyEvent evt) {
@@ -11,6 +14,7 @@ public class KeyDownListener implements KeyListener {
 
     @Override
     public void keyReleased(KeyEvent evt) {
+        logger.info("Released Key: " + evt.getKeyChar());
     }
 
     @Override
