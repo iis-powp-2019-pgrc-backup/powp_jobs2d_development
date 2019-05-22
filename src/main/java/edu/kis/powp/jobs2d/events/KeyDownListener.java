5e1521e0d9b63df638470c234b344f5986d161db
dettlaffb <209295@edu.p.lodz.pl>
2019-05-22 15:14:03 +0200

    Dodanie klasy KeyDownListener

---
+++ src/main/java/edu/kis/powp/jobs2d/events/KeyDownListener.java
@@ -0,0 +1,21 @@
+package edu.kis.powp.jobs2d.events;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class KeyDownListener implements KeyListener {
+
+    @Override
+    public void keyPressed(KeyEvent evt) {
+    }
+
+    @Override
+    public void keyReleased(KeyEvent evt) {
+    }
+
+    @Override
+    public void keyTyped(KeyEvent evt) {
+
+    }
+
+}
