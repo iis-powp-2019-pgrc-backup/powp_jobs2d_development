64763f2c85d35f7841654ad3ded4d5cb7ef3c81b
JHinge <zawiasa.jakub@gmail.com>
2019-05-23 15:21:02 +0200

    mouse drawing moved to separate class

--- src/main/java/edu/kis/powp/jobs2d/features/DrawerFeature.java
+++ src/main/java/edu/kis/powp/jobs2d/features/DrawerFeature.java
@@ -2,14 +2,6 @@ package edu.kis.powp.jobs2d.features;
 
 import edu.kis.powp.appbase.Application;
 import edu.kis.powp.jobs2d.events.SelectClearPanelOptionListener;
-
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
-import java.util.logging.Logger;
-
-import javax.swing.JPanel;
-import javax.swing.SwingUtilities;
-
 import edu.kis.legacy.drawer.panel.DrawPanelController;
 
 public class DrawerFeature {
@@ -31,27 +23,6 @@ public class DrawerFeature {
 		drawerController.initialize(application.getFreePanel());
 	}
 
-	public static void addMouseDrawing(Application app, Logger logger) {
-		JPanel panel = app.getFreePanel();
-		int width, height;
-		width = panel.getWidth();
-		height = panel.getHeight();
-		panel.addMouseListener(new MouseAdapter() {
-
-			@Override
-			public void mouseReleased(MouseEvent e) {
-				if (SwingUtilities.isLeftMouseButton(e)) {
-					DriverFeature.getDriverManager().getCurrentDriver().operateTo(e.getX() - width / 2,
-							e.getY() - height / 2);
-					logger.warning("Operate to X:" + (e.getX() - width / 2) + " Y:" + (e.getY() - height / 2));
-				} else if (SwingUtilities.isRightMouseButton(e)) {
-					DriverFeature.getDriverManager().getCurrentDriver().setPosition(e.getX() - width / 2,
-							e.getY() - height / 2);
-					logger.warning("Set Position X:" + (e.getX() - width / 2) + " Y:" + (e.getY() - height / 2));
-				}
-			}
-		});
-	}
 
 	/**
 	 * Get controller of application drawing panel.
