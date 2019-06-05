239412c7abc5b8a2dc053fa2c4a056c0c53be8da
Matix97 <mateuszBaranskiP@gmail.com>
2019-06-05 14:27:53 +0200

    Added units

--- src/main/java/edu/kis/powp/jobs2d/logger/UsageLogger.java
+++ src/main/java/edu/kis/powp/jobs2d/logger/UsageLogger.java
@@ -2,14 +2,17 @@ package edu.kis.powp.jobs2d.logger;
 
 public class UsageLogger {
 
-	public static int totalUsage = 0;
-	public static int totalConsumption = 0;
+	public static float totalUsage = 0;
+	public static float totalConsumption = 0;
 	public static int oldX = 0;
 	public static int oldY = 0;
 	
 	public static void increaseConsumption( int x, int y ) {
 		totalConsumption += Math.sqrt( (oldX - x)*(oldX - x) + (oldY - y)*(oldY - y) );
+		totalConsumption = totalConsumption / 115;
+		
 		totalUsage += Math.sqrt( (oldX - x)*(oldX - x) + (oldY - y)*(oldY - y) );
+		totalUsage = totalUsage/10;
 		
 		oldX = x;
 		oldY = y;
@@ -17,14 +20,15 @@ public class UsageLogger {
 	
 	public static void setPosition( int x, int y ) {
 		totalUsage += Math.sqrt( (oldX - x)*(oldX - x) + (oldY - y)*(oldY - y) );
+		totalUsage = totalUsage/10;
 		
 		oldX = x;
 		oldY = y;
 	}
 	
 	public static String showInfo() {
-		return "Total consumption: " + Integer.toString( totalConsumption ) + "\n" +
-				"Total usage: " + Integer.toString( totalUsage ) + "\n" ;
+		return "Ink consumption: " + Float.toString( totalConsumption ) + " l\n" +
+				"Head usage: " + Float.toString( totalUsage ) + "cm\n" ;
 	}
 	
 }
