dc961029ba0a48d224138063856df9eea8b25b68
Sebastian Madejski <209388@edu.p.lodz.pl>
2019-06-05 14:44:48 +0200

    use commands instead of hardcoded driver operation in MyMouseDrawingListener

--- src/main/java/edu/kis/powp/jobs2d/eventListener/MyMouseDrawingListener.java
+++ src/main/java/edu/kis/powp/jobs2d/eventListener/MyMouseDrawingListener.java
@@ -1,14 +1,15 @@
 package edu.kis.powp.jobs2d.eventListener;
 
 import edu.kis.powp.appbase.Application;
+import edu.kis.powp.jobs2d.command.*;
 import edu.kis.powp.jobs2d.features.DriverFeature;
 
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.util.ArrayList;
 
 public class MyMouseDrawingListener implements MouseListener {
     private Application app;
-
 
     public MyMouseDrawingListener(Application app) {
         this.app = app;
@@ -17,15 +18,18 @@ public class MyMouseDrawingListener implements MouseListener {
 
     @Override
     public void mouseClicked(MouseEvent e) {
-        if (e.getButton() == MouseEvent.BUTTON1) {
-            DriverFeature.getDriverManager().getCurrentDriver()
-                    .operateTo(e.getX() - app.getFreePanel().getWidth() / 2, e.getY() - app.getFreePanel().getHeight() / 2);
-        } else if (e.getButton() == MouseEvent.BUTTON3) {
-            DriverFeature.getDriverManager().getCurrentDriver()
-                    .setPosition(e.getX() - app.getFreePanel().getWidth() / 2, e.getY() - app.getFreePanel().getHeight() / 2);
+        DriverCommand commandToRecord = null;
+        int x = e.getX() - app.getFreePanel().getWidth() / 2;
+        int y = e.getY() - app.getFreePanel().getHeight() / 2;
 
+        if (e.getButton() == MouseEvent.BUTTON1) {
+            commandToRecord = new OperateToCommand(x, y);
+        } else if (e.getButton() == MouseEvent.BUTTON3) {
+            commandToRecord = new SetPositionCommand(x, y);
         }
 
+        if(commandToRecord != null)
+            commandToRecord.execute(DriverFeature.getDriverManager().getCurrentDriver());
 
     }
 
