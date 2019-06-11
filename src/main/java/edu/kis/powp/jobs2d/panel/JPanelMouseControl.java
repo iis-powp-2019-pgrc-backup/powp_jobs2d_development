f46ac6556bfb024ece13b2c5e1b38cd1a1dac9ac
Tobiasz Skorża <209458@edu.p.lodz.pl>
2019-06-11 21:54:54 +0200

    Added JPanelMouseControl to get data where mouse position is

---
+++ src/main/java/edu/kis/powp/jobs2d/panel/JPanelMouseControl.java
@@ -0,0 +1,80 @@
+package edu.kis.powp.jobs2d.panel;
+
+import edu.kis.powp.jobs2d.drivers.DriverManager;
+import edu.kis.powp.jobs2d.features.MoveFeature;
+
+import javax.swing.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+
+
+public class JPanelMouseControl {
+    private static JPanel jPanel = null;
+
+    public static JPanel getjPanel() {
+        return jPanel;
+    }
+
+    private static DriverManager driverManager = null;
+
+    public static void engage(JPanel jPanel, DriverManager driverManager)
+    {
+        setjPanel(jPanel);
+        setDriverManager(driverManager);
+        startListener();
+    }
+
+    private static void setjPanel(JPanel currentJPanel) {
+        jPanel = currentJPanel;
+    }
+
+
+    private static void setDriverManager(DriverManager currentDriverManager) {
+        driverManager = currentDriverManager;
+    }
+
+
+    private static void startListener() {
+        jPanel.addMouseListener(new MouseListener() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                System.out.println("mouseClicked, X=" + e.getX() + " ,Y=" + e.getY() + " ,Count=" + e.getClickCount());
+            }
+
+            @Override
+            public void mousePressed(MouseEvent e) {
+                System.out.println("mousePressed");
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e) {
+                System.out.println("mouseReleased");
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                System.out.println("mouseEntered");
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+                System.out.println("mouseExited");
+            }
+
+        });
+
+        jPanel.addMouseMotionListener(new MouseMotionListener() {
+            @Override
+            public void mouseDragged(MouseEvent e) {
+                jPanel.setBounds(e.getX()-120, e.getY()-120, 500, 500);
+            }
+
+            @Override
+            public void mouseMoved(MouseEvent e) {
+
+            }
+        });
+
+    }
+}
