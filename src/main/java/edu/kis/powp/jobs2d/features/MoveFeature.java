ec7a7ad0ff374580c6fbf4f25b8de489fcf49951
Krystian Nowak <krystianowak1@gmail.com>
2019-06-11 15:42:30 +0200

    Logger info added

--- src/main/java/edu/kis/powp/jobs2d/features/MoveFeature.java
+++ src/main/java/edu/kis/powp/jobs2d/features/MoveFeature.java
@@ -1,10 +1,14 @@
 package edu.kis.powp.jobs2d.features;
 
+import java.util.logging.Logger;
+
 import edu.kis.powp.appbase.Application;
 
 
 public class MoveFeature {
 
+	private final static Logger logger = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
+	
 	private static Application app;
 	public static int x=0;
 	public static int y=0; 
@@ -23,30 +27,35 @@ public class MoveFeature {
 	public static  void setX() {
 		MoveFeature.x+=20;
 		System.out.println("x: " + MoveFeature.x);
+		logger.info("x:" + MoveFeature.x);
+		
 	}
 
 
 	public static void setY() {
 		MoveFeature.y+=20;
 		System.out.println("y: " + MoveFeature.y);
+		logger.info("y: " + MoveFeature.y);
 	}
 	
 	public static  void setXX() {
 		MoveFeature.x-=20;
 		System.out.println("x: " + MoveFeature.x);
+		logger.info("x: " + MoveFeature.x);
 	}
 
 
 	public static void setYY() {
 		MoveFeature.y-=20;
 		System.out.println("y: " + MoveFeature.y);
+		logger.info("y: " + MoveFeature.y);
 	}
 
 
 	public static void reset() {
 		MoveFeature.y=0;
 		MoveFeature.x=0;
-		System.out.println("x: " + MoveFeature.x + " " + "y: " + MoveFeature.y);
+		logger.info("x: " + MoveFeature.x + " y: " + MoveFeature.y);
 	}
 	
 
