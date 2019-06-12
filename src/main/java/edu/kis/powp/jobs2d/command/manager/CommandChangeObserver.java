8f4060fe6f38fbcfc9e1be254470715ab98cba8f
Szymon Sobieszczyk <esesinho@gmail.com>
2019-06-12 16:17:29 +0200

    Dodano obserwer

---
+++ src/main/java/edu/kis/powp/jobs2d/command/manager/CommandChangeObserver.java
@@ -0,0 +1,13 @@
+package edu.kis.powp.jobs2d.command.manager;
+
+import edu.kis.powp.jobs2d.command.manager.CommandHistory;
+import edu.kis.powp.jobs2d.features.CommandsFeature;
+import edu.kis.powp.observer.Subscriber;
+
+public class CommandChangeObserver  implements Subscriber {
+    @Override
+    public void update() {
+        if(CommandHistory.logMode)
+            CommandHistory.addEntry(CommandsFeature.getDriverCommandManager().getCommandList(),CommandsFeature.getDriverCommandManager().getCommandName());
+    }
+}
