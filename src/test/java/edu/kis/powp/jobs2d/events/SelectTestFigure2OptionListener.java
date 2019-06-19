62297b40fba1c5cbf14f78a5e5b740fdf5b75d53
Sebastian Madejski <209388@edu.p.lodz.pl>
2019-06-19 15:05:23 +0200

    intercept record from figures

--- src/test/java/edu/kis/powp/jobs2d/events/SelectTestFigure2OptionListener.java
+++ src/test/java/edu/kis/powp/jobs2d/events/SelectTestFigure2OptionListener.java
@@ -4,6 +4,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import edu.kis.powp.jobs2d.drivers.DriverManager;
+import edu.kis.powp.jobs2d.drivers.RecordDriver;
 import edu.kis.powp.jobs2d.magicpresets.FiguresJoe;
 
 public class SelectTestFigure2OptionListener implements ActionListener {
@@ -16,6 +17,6 @@ public class SelectTestFigure2OptionListener implements ActionListener {
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		FiguresJoe.figureScript2(driverManager.getCurrentDriver());
+		FiguresJoe.figureScript2(new RecordDriver(driverManager.getCurrentDriver()));
 	}
 }
--- src/test/java/edu/kis/powp/jobs2d/events/SelectTestFigureOptionListener.java
+++ src/test/java/edu/kis/powp/jobs2d/events/SelectTestFigureOptionListener.java
@@ -4,6 +4,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import edu.kis.powp.jobs2d.drivers.DriverManager;
+import edu.kis.powp.jobs2d.drivers.RecordDriver;
 import edu.kis.powp.jobs2d.magicpresets.FiguresJoe;
 
 public class SelectTestFigureOptionListener implements ActionListener {
@@ -16,6 +17,6 @@ public class SelectTestFigureOptionListener implements ActionListener {
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		FiguresJoe.figureScript1(driverManager.getCurrentDriver());
+		FiguresJoe.figureScript1(new RecordDriver(driverManager.getCurrentDriver()));
 	}
 }
