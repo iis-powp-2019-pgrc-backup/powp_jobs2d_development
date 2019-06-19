e7471055ae2ec8b693b3c00abebc9a5e70b01400
Sebastian Madejski <209388@edu.p.lodz.pl>
2019-06-19 15:15:45 +0200

    Revert "intercept record from figures"
    This reverts commit 62297b40fba1c5cbf14f78a5e5b740fdf5b75d53.

--- src/test/java/edu/kis/powp/jobs2d/events/SelectTestFigureOptionListener.java
+++ src/test/java/edu/kis/powp/jobs2d/events/SelectTestFigureOptionListener.java
@@ -4,7 +4,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import edu.kis.powp.jobs2d.drivers.DriverManager;
-import edu.kis.powp.jobs2d.drivers.RecordDriver;
 import edu.kis.powp.jobs2d.magicpresets.FiguresJoe;
 
 public class SelectTestFigureOptionListener implements ActionListener {
@@ -17,6 +16,6 @@ public class SelectTestFigureOptionListener implements ActionListener {
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		FiguresJoe.figureScript1(new RecordDriver(driverManager.getCurrentDriver()));
+		FiguresJoe.figureScript1(driverManager.getCurrentDriver());
 	}
 }
