cb4fc13a9a97d327074c44afc58046a2ebeed3ee
Sebastian Madejski <209388@edu.p.lodz.pl>
2019-06-19 15:02:56 +0200

    add RecordDriver

---
+++ src/main/java/edu/kis/powp/jobs2d/drivers/RecordDriver.java
@@ -0,0 +1,27 @@
+package edu.kis.powp.jobs2d.drivers;
+
+import edu.kis.powp.jobs2d.Job2dDriver;
+import edu.kis.powp.jobs2d.command.OperateToCommand;
+import edu.kis.powp.jobs2d.command.SetPositionCommand;
+import edu.kis.powp.jobs2d.features.RecorderFeature;
+
+public class RecordDriver implements Job2dDriver {
+
+    private Job2dDriver nextDriver;
+
+    public RecordDriver(Job2dDriver nextDriver) {
+        this.nextDriver = nextDriver;
+    }
+
+    @Override
+    public void setPosition(int x, int y) {
+        if(RecorderFeature.isRecordInProgress()) RecorderFeature.record(new SetPositionCommand(x, y));
+        if(nextDriver != null) nextDriver.setPosition(x, y);
+    }
+
+    @Override
+    public void operateTo(int x, int y) {
+        if(RecorderFeature.isRecordInProgress()) RecorderFeature.record(new OperateToCommand(x, y));
+        if(nextDriver != null) nextDriver.operateTo(x, y);
+    }
+}
