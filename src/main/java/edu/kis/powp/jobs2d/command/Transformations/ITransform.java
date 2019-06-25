af8dd7ff1dbf2774843364b1c2042131cad9c1e6
194930 <194930@edu.p.lodz.pl>
2019-06-25 09:49:41 +0200

    Added ITransform

---
+++ src/main/java/edu/kis/powp/jobs2d/command/Transformations/ITransform.java
@@ -0,0 +1,8 @@
+package edu.kis.powp.jobs2d.command.Transformations;
+
+import edu.kis.powp.jobs2d.command.ICompoundCommand;
+
+public interface ITransform {
+
+    public ICompoundCommand performTransformation();
+}
